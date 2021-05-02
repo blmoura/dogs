5dcc520256155ef40ec3fd80c89d47a1ee344e16
Bruno Moura <brunomoura.blm@gmail.com>
2021-05-02 16:01:33 -0300

    Hook useForm para validaçao de formulario

--- src/Components/Forms/Input.js
+++ src/Components/Forms/Input.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import styles from './Input.module.css';
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input type={type} className={styles.input} id={name} name={name} />
-      <p className={styles.error}>Error</p>
+      <input
+        type={type}
+        className={styles.input}
+        id={name}
+        name={name}
+        onChange={onChange}
+        value={value}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
