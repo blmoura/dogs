20c4002a0b518e283fa6bd4fbc83ac2783f97e0a
Bruno Moura <brunomoura.blm@gmail.com>
2021-05-05 22:18:11 -0300

    Feed - Inicio do desenvolvimento do Feed

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import FeedModal from './FeedModal';
+import FeedPhotos from './FeedPhotos';
 
 const Feed = () => {
-  return <div>FEed</div>;
+  return (
+    <div>
+      <FeedModal />
+      <FeedPhotos />
+    </div>
+  );
 };
 
 export default Feed;
