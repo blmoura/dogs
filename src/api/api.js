699d136764800daa2da71d3b02152a613b00a3dc
Bruno Moura <brunomoura.blm@gmail.com>
2021-05-06 22:47:24 -0300

    Photo Delete - Adicionando funcionalidade pro author do post deletar a foto postada

--- src/api/api.js
+++ src/api/api.js
@@ -97,3 +97,16 @@ export function COMMENT_POST(id, body, token) {
     },
   };
 }
+
+export function PHOTO_DELETE(id, token) {
+  return {
+    url: `${API_URL}/api/photo/${id}`,
+    options: {
+      method: 'DELETE',
+      cache: 'no-store',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    },
+  };
+}
